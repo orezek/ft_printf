91d9370892ffb1ab6d7baf69b1606a0684dfc521
O.R <orezek@seznam.cz>
2023-11-09 22:46:21 +0100

    Update: add ft_putchar and ft_putstr

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 15:27:30 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/09 16:28:42 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/09 22:44:05 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,11 @@ int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	va_list	args_cpy;
+	int	chars_printed;
+	int	*ptr_chars_printed;
 
-	va_copy(args_cpy, args);
+	chars_printed = 0;
+	ptr_chars_printed = &chars_printed;
 	va_start(args, format);
 
 	while (*format)
@@ -26,14 +29,12 @@ int	ft_printf(const char *format, ...)
 		{
 			format++;
 			if (*format == 's')
-				//ft_putstr(va_arg(args, char *));
-				printf("%s", va_arg(args, char *));
+				ft_putstr(va_arg(args, char *), ptr_chars_printed);
 			else if (*format == 'd')
 				//ft_putnbr(va_arg(args, int));
 				printf("%d", va_arg(args, int));
 			else if (*format == 'c')
-				//ft_putchar(va_arg(args, int));
-				printf("%c", va_arg(args, int));
+				ft_putchar(va_arg(args, int), ptr_chars_printed);
 			else if (*format == 'p')
 				//ft_putnbr(va_arg(args, int *));
 				printf("%p", va_arg(args, void *));
@@ -50,16 +51,14 @@ int	ft_printf(const char *format, ...)
 				//ft_putnbr(va_arg(args, unsigned int));
 				printf("%X", va_arg(args, unsigned int));
 			else if (*format == '%')
-				//ft_putchar('%');
-				printf("%%");
+				ft_putchar('%', ptr_chars_printed);
 			else
 				return (-1);
 		}
 		else
-			//ft_putchar(*format);
-			printf("%c", *format);
+			ft_putchar(*format, ptr_chars_printed);
 		format++;
 	}
 	va_end(args);
-	return (0);
+	return (chars_printed);
 }
