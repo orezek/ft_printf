65c29b9db68034aea868c3eb71a15085ee21e674
O.R <orezek@seznam.cz>
2023-11-11 11:44:30 +0100

    Add: add ft_unsigned_putnbr and ft_unsgined_itoa functions

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:41:29 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/09 22:53:52 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/11 11:40:03 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,13 @@
 # include <stdio.h>
 # include <stdarg.h>
 # include <unistd.h>
+# include <stdlib.h>
 
 int		ft_printf(const char *format, ...);
 void	ft_putchar(char c, int *ptr_chars_printed);
 void	ft_putstr(char *str, int *ptr_chars_printed);
+void	ft_putnbr(int n, int *ptr_chars_printed);
+char	*ft_itoa(int n);
+char	*ft_unsigned_itoa(int n);
+void	ft_unsigned_putnbr(int n, int *ptr_chars_printed);
 #endif
