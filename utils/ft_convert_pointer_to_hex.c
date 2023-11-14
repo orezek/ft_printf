5f624b9357b5c2f4f2358af7773a6c04f6c8ec48
O.R <orezek@seznam.cz>
2023-11-14 16:59:42 +0100

    Update: add malloc and NULL pointer check to convert_pointer function

--- utils/ft_convert_pointer_to_hex.c
+++ utils/ft_convert_pointer_to_hex.c
@@ -6,7 +6,7 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 18:10:11 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/14 16:18:16 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/14 16:56:11 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,19 @@ char	*ft_convert_pointer_to_hex(u_int64_t bin_n)
 	int		hex_len;
 	int		index;
 
+	if (bin_n == 0)
+		return (ft_strdup("0x0"));
 	hex_len = ft_hexlen(bin_n);
 	hex_str = (char *)malloc(sizeof(char) * (hex_len + 3));
+	if (!hex_str)
+		return (NULL);
 	*(hex_str + (hex_len + 2)) = '\0';
 	ft_strcpy(hex_digits, "0123456789abcdef");
 	index = hex_len + 1;
-	while (hex_len > 0)
+	while (hex_len-- > 0)
 	{
 		hex_str[index--] = hex_digits[bin_n & 0xF];
 		bin_n >>= 4;
-		hex_len--;
 	}
 	hex_str[0] = '0';
 	hex_str[1] = 'x';
