506d22362c424577a4d275b63700f6345b6d7ee6
O.R <orezek@seznam.cz>
2023-11-15 20:32:13 +0100

    Fix: fix formatting to comply with norm

--- utils/ft_puthex.c
+++ utils/ft_puthex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_puthex.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
+/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:04:37 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/15 15:04:03 by orezek           ###   ########.fr       */
+/*   Updated: 2023/11/15 20:29:13 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ void	ft_puthex(int ptr_address, int *ptr_chars_printed)
 	char	*ptr_address_str;
 
 	ptr_address_str = NULL;
-
 	ptr_address_str = ft_convert_number_to_hex((uint64_t)ptr_address);
 	ft_putstr(ptr_address_str, ptr_chars_printed);
 	free(ptr_address_str);
