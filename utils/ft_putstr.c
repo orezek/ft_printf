1227229a4e6c5299bcf7995cabe8d3f3eb8277d2
Oldrich Rezek <orezek@seznam.cz>
2023-11-15 15:36:45 +0100

    Test: update test file

--- utils/ft_putstr.c
+++ utils/ft_putstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
+/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 21:51:45 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/09 21:53:37 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/15 15:33:13 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 void	ft_putstr(char *str, int *ptr_chars_printed)
 {
-	while (*str)
-		ft_putchar(*(str++), ptr_chars_printed);
+	if (str != NULL)
+	{
+		while (*str)
+			ft_putchar(*(str++), ptr_chars_printed);
+	}
+	else
+		ft_putstr("(null)", ptr_chars_printed);
 }
