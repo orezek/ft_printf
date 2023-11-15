f2e4efcaa5267c60c50082d9146e97eb26ae3138
O.R <orezek@seznam.cz>
2023-11-15 23:58:58 +0100

    Fix: change argument datat type to uint64_t

--- utils/ft_hexlen.c
+++ utils/ft_hexlen.c
@@ -6,13 +6,13 @@
 /*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 18:15:46 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/13 18:15:51 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/15 21:22:02 by aldokezer        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
-int	ft_hexlen(u_int64_t bin_n)
+int	ft_hexlen(uint64_t bin_n)
 {
 	int	i;
 
