26ebbe0cdd33ad22386046ab53bc16b17c215a6d
Oldrich Rezek <orezek@seznam.cz>
2023-11-16 10:58:33 +0100

    Udpate: cleanup project folder

--- tst_printf.c
+++ tst_printf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   tst_printf.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aldokezer <aldokezer@student.42.fr>        +#+  +:+       +#+        */
+/*   By: orezek <orezek@student.42prague.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:41:11 by aldokezer         #+#    #+#             */
-/*   Updated: 2023/11/16 00:17:19 by aldokezer        ###   ########.fr       */
+/*   Updated: 2023/11/16 10:50:54 by orezek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 // cc -Wall -Wextra -Werror tst_printf.c ft_printf.c utils/ft_convert_number_to_hex.c utils/ft_strlen.c utils/ft_hexlen.c utils/ft_strcpy.c utils/ft_putchar.c utils/ft_putnbr.c utils/ft_putptr.c utils/ft_putstr.c utils/ft_putnbr_unsigned.c utils/ft_convert_pointer_to_hex.c utils/ft_itoa.c utils/ft_unsigned_itoa.c utils/ft_puthex.c utils/ft_puthex_upper.c utils/ft_putstr_upper.c utils/ft_toupper.c utils/ft_strdup.c utils/ft_printf_short.c && ./a.out
 int	main(void)
 {
-	int	result1;
-	int	result2;
+	//int	result1;
+	//int	result2;
 	char *ptr;
 	char str[] = "ahoj";
 	ptr = str;
@@ -24,8 +24,8 @@ int	main(void)
 	//result2 = printf(" %x ", -1);
 	//write(1, "\n", 1);
 	//printf(" %c %c %c ", '0', 0, '1');
-	ft_printf("%x\n", -10);
-	printf("%x\n",-10);
+	printf("%d", ft_printf(" %p %p ", 0, 0));
+	printf("%p %p", (void *)0, (void *)0);
 	//printf("printf: %d\n", result2);
 	return (0);
 }
